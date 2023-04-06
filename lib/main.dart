0f977da1ab8189a50a511d482a414d45842c8f6a
rnikkhooy <rnikkhooy@gmail.com>
2023-04-06 15:04:33 +0330

    first msg

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:test100/test100.dart';
+
+void main() {
+  runApp(MyProg1());
+}
+
+class MyProg1 extends StatelessWidget {
+  const MyProg1({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Pr(),
+    );
+  }
+}
+
